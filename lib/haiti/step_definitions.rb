c4212b1120ce033b24c9a27b815540869e39a147
Josh Cheek <josh.cheek@gmail.com>
2013-06-06 12:52:08 -0500

    Abstraction to allow multiple declarations for one block

--- lib/haiti/step_definitions.rb
+++ lib/haiti/step_definitions.rb
@@ -1,15 +1,50 @@
-Given('the file "$filename" "$body"')                { |filename, body|       Haiti::CommandLineHelpers.write_file filename, eval_curlies(body) }
-Given('the file "$filename":')                       { |filename, body|       Haiti::CommandLineHelpers.write_file filename, eval_curlies(body) }
-Given('the stdin content "$content"')                { |content|              @stdin_data = eval_curlies(content) }
-Given('the stdin content:')                          { |content|              @stdin_data = eval_curlies(content) }
-When('I run "$command"')                             { |command|              @last_executed = Haiti::CommandLineHelpers.execute command, @stdin_data }
-When("I run '$command'")                             { |command|              @last_executed = Haiti::CommandLineHelpers.execute command, @stdin_data }
-Then(/^(stderr|stdout) is:$/)                        { |stream_name, output|  @last_executed.send(stream_name).chomp.should == eval_curlies(output) }
-Then(/^(stderr|stdout) is (["'])(.*?)\2$/)           { |stream_name, _, output| @last_executed.send(stream_name).chomp.should == eval_curlies(output) }
-Then(/^(stderr|stdout) is empty$/)                   { |stream_name|          @last_executed.send(stream_name).should == '' }
-Then(/^(stderr|stdout) is not empty$/)               { |stream_name|          @last_executed.send(stream_name).chomp.should_not be_empty }
-Then(/^(stderr|stdout) includes "(.*?)"$/)           { |stream_name, content| @last_executed.send(stream_name).should include eval_curlies(content) }
-Then(/^(stderr|stdout) includes:$/)                  { |stream_name, content| @last_executed.send(stream_name).should include eval_curlies(content) }
-Then('the exit status is $status')                   { |status|               @last_executed.exitstatus.to_s.should == status }
-Then(/^(stderr|stdout) does not include "(.*?)"$/)   { |stream_name, content| @last_executed.send(stream_name).should_not include eval_curlies(content) }
-Then(/^(stderr|stdout) does not include:$/)          { |stream_name, content| @last_executed.send(stream_name).should_not include eval_curlies(content) }
+define_steps = lambda do |*matchers, &definition|
+  matchers.each { |matcher| Given matcher, &definition }
+end
+
+define_steps.call(
+  'the file "$filename" "$body"',
+  'the file "$filename":'
+) { |filename, body| Haiti::CommandLineHelpers.write_file filename, eval_curlies(body) }
+
+define_steps.call(
+  'the stdin content "$content"',
+  'the stdin content:'
+) { |content| @stdin_data = eval_curlies(content) }
+
+
+define_steps.call(
+  'I run "$command"',
+  "I run '$command'",
+) { |command| @last_executed = Haiti::CommandLineHelpers.execute command, @stdin_data }
+
+define_steps.call(
+  /^(stderr|stdout) is:$/,
+  /^(stderr|stdout) is "(.*?)"$/,
+  /^(stderr|stdout) is '(.*?)'$/
+) { |stream_name, output|  @last_executed.send(stream_name).chomp.should == eval_curlies(output) }
+
+define_steps.call(
+  /^(stderr|stdout) is empty$/
+) { |stream_name| @last_executed.send(stream_name).should == '' }
+
+define_steps.call(
+  /^(stderr|stdout) is not empty$/
+) { |stream_name| @last_executed.send(stream_name).chomp.should_not be_empty }
+
+define_steps.call(
+  /^(stderr|stdout) includes "(.*?)"$/
+) { |stream_name, content| @last_executed.send(stream_name).should include eval_curlies(content) }
+
+define_steps.call(
+  /^(stderr|stdout) includes:$/
+) { |stream_name, content| @last_executed.send(stream_name).should include eval_curlies(content) }
+
+define_steps.call(
+  'the exit status is $status'
+) { |status| @last_executed.exitstatus.to_s.should == status }
+
+define_steps.call(
+  /^(stderr|stdout) does not include "(.*?)"$/,
+  /^(stderr|stdout) does not include:$/
+) { |stream_name, content| @last_executed.send(stream_name).should_not include eval_curlies(content) }
